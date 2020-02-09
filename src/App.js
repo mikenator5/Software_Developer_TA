2b746503bb6015605ac22e3b6c7bbd5cc0a513fb
mikenator5 <43967728+mikenator5@users.noreply.github.com>
2020-02-08 17:35:41 -0700

    New Changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import './index.css';
 
 class App extends React.Component {
 
+  state = {
+    files: [],
+  }
+
   dropHandler = (e) => {
     e.preventDefault();
     console.log("Files dropped");
     
+    if (e.dataTransfer.items) {
+      for (let i = 0; i < e.dataTransfer.items.length; i++) {
+
+        // If dropped items aren't files, reject them
+        if (e.dataTransfer.items[i].kind === 'file') {
+          var file = e.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
+          this.setState({ files: [...this.state.files, file.name]  })
+        }
+        
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < e.dataTransfer.files.length; i++) {
+        console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+        
+      }
+    }
+
   }
 
   dragOverHandler = (e) => {
@@ -19,15 +42,22 @@ class App extends React.Component {
     return (
       <div className="container">
           
-          <ProgressBar variant="danger" now={100} />
-
+          <div>
+            <ProgressBar variant="danger" now={100} />
+            <ul>
+              {this.state.files.map((item, index) => <li key={index} className="item" >{item}</li>)}
+            </ul>
+          </div>
+          
           <div id="bot">
             <div id="drop_zone" onDrop={this.dropHandler} onDragOver={this.dragOverHandler} >
-              <p>Drag files here or Browse</p>
+
+              <p id="drop_zone_p">Drag files here or Browse</p>
               <input type="file" id="file" />
 
             </div>
           </div>
+          
       </div>
     );
   }
